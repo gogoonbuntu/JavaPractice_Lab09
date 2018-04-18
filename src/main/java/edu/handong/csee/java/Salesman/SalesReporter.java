c01c415d5ec22a5658d9ada3cd795c2d5f2346db
GGBT <tmddud333@naver.com>
2018-04-18 18:22:38 +0900

    done!!

--- src/main/java/edu/handong/csee/java/Salesman/SalesReporter.java
+++ src/main/java/edu/handong/csee/java/Salesman/SalesReporter.java
@@ -1,6 +1,6 @@
 package edu.handong.csee.java.Salesman;
 
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * @author SeungYoung Jung
@@ -13,14 +13,10 @@ public class SalesReporter {
 	
 	private double high; //to save highest sales.
 	private double aver; //to save average value.
-	private SalesAsso[] team; //to save team members as an array
-	private int numSalesman; //to save members of the salesman
-	
+	private int numSalesman=0; //to save members of the salesman
+	private ArrayList<SalesAsso> team = new ArrayList<SalesAsso>(); // declare an arraylist, which has the size of the numSalesman variable.
 	public SalesReporter() { 
-		//to make a construct. the whole class is a program that reports the sales result.
-		System.out.println("Enter number of salesman :");
-		Scanner MS = new Scanner(System.in);
-		numSalesman = MS.nextInt(); //saved the number of salesmain in the variable.
+		//to make a construct. 
 	}
 	
 	
@@ -36,46 +32,57 @@ public class SalesReporter {
 	
 	public void getData()
 	{
-		team = new SalesAsso[numSalesman]; // declare an array, which has the size of the numSalesman variable.
-		for(int i=0; i < numSalesman; i++)
+		boolean done=false;
+		while(!done)//loop starts, when the user type "N" to say no more salesman, it stops.
 		{
 			Scanner mySC = new Scanner(System.in);
-			
-			System.out.println("Get data for asociate number"+(i+1));
+			System.out.println("Get data for asociate number"+(team.size()+1));
 			
 			System.out.println("Enter name");
 			String name = mySC.nextLine();//get a String for name.
-			
 			System.out.println("Enter Sales:");
 			double sales = mySC.nextDouble();//get a double for sales.
 			
 			SalesAsso mySA = new SalesAsso();//declare a new SalesAsso class.
 			mySA.setmName(name);//save name in the object.
 			mySA.setSales(sales);//save sales in the object.
-			team[i]=mySA;//put the object into the array.
+			team.add(mySA);//add mySA to arraylist.
+			numSalesman++;//plus one count.
+			
+			System.out.println("-If you want to add more Salesman, type 'Y', if not type 'N'. current number : "+numSalesman);
+			//decide whether to get more salesman or not.
+			//mySC.next();//get input.
+			String yn = "n";//get input.
+			//yn = mySC.nextLine();//get input.
+			mySC.nextLine();
+			System.out.println("yn = "+yn);
+			yn = mySC.nextLine();
+			System.out.println("2yn = "+yn);
+			if(yn.equalsIgnoreCase("n")) {mySC.close();done=true;};//if it's N/n, the loop stops.
 		}
+		
 	}
 	
 	public void Aver() {
 		double sum = 0;
-		for(int i=0;i<team.length; i++)
+		for(int i=0;i<team.size(); i++)
 		{
-			double sales = team[i].getSales(); //read the sales from the array.
+			double sales = team.get(i).getSales(); //read the sales from the array.
 			sum += sales;//sequentially accumulate the value in the sum. 
 		}
-		aver=sum/team.length;//divide it into average value.
+		aver=sum/team.size();//divide it into average value.
 	}
 	
 	public void High() {
 
-		for(int i=0;i<team.length-1; i++)//because we keep compare to the next one, the last sequence should be omitted.
+		for(int i=0;i<team.size()-1; i++)//because we keep compare to the next one, the last sequence should be omitted.
 		{
-			high = team[i].getSales();
+			high = team.get(i).getSales();
 			//originally let high equals to the first one. 
 			//without this comment, if the first salesman has the highest value, computer wouldn't know it.
-			if(team[i].getSales()<team[i+1].getSales())//compare to the next salesman.
+			if(team.get(i).getSales()<team.get(i+1).getSales())//compare to the next salesman.
 			{
-				high = team[i+1].getSales();//let the next one be the highest salesman.
+				high = team.get(i+1).getSales();//let the next one be the highest salesman.
 			}
 		}
 	}
@@ -87,24 +94,24 @@ public class SalesReporter {
 		System.out.println("");//to make the result look clear.
 		System.out.println("The following had the highest sales, ");
 		
-		for(int i=0;i<team.length; i++)
+		for(int i=0;i<team.size(); i++)
 		{
-			if(team[i].getSales()==high)//find out the highest salesman.
+			if(team.get(i).getSales()==high)//find out the highest salesman.
 			{
-				System.out.println("Name:" + team[i].getmName());//shows the name.
-				System.out.println("Sales: $" + team[i].getSales());//shows the sales.
-				System.out.println((team[i].getSales()-aver)+" above the average.");//shows the differences between the sales and the average.
+				System.out.println("Name:" + team.get(i).getmName());//shows the name.
+				System.out.println("Sales: $" + team.get(i).getSales());//shows the sales.
+				System.out.println((team.get(i).getSales()-aver)+" above the average.");//shows the differences between the sales and the average.
 			}
 		}System.out.println("");//to make the result look clear.
 		System.out.println("The rest performed as follows");
-		for(int i=0;i<team.length; i++)
+		for(int i=0;i<team.size(); i++)
 		{
-			if(team[i].getSales()!=high)
+			if(team.get(i).getSales()!=high)
 			{
-				System.out.println("Name:" + team[i].getmName());//shows the name.
-				System.out.println("Sales: $" + team[i].getSales());//shows the sales.
-				if(aver>team[i].getSales()) System.out.println("$"+(aver-team[i].getSales())+" below the average");
-				else System.out.println("$"+(team[i].getSales()-aver)+" above the average");
+				System.out.println("Name:" + team.get(i).getmName());//shows the name.
+				System.out.println("Sales: $" + team.get(i).getSales());//shows the sales.
+				if(aver>team.get(i).getSales()) System.out.println("$"+(aver-team.get(i).getSales())+" below the average");
+				else System.out.println("$"+(team.get(i).getSales()-aver)+" above the average");
 				//compare the sales to average, shows out the differences.
 			}
 		}
